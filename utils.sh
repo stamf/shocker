083811a524c9d1929af36aee3f24c1de67ad2f30
Yoshua Wuyts <i@yoshuawuyts.com>
2015-12-03 17:35:57 +1100

    shocker -> shocker-*
    - split out into cleanup,commit,export,logs,name,route,stop
    
    Signed-off-by: Yoshua Wuyts <i@yoshuawuyts.com>
    
    Closes #

--- utils.sh
+++ utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-btrfs_path='/var/shocker' && cgroups='cpu,cpuacct,memory'
+btrfs_path='/var/shocker'
+cgroups='cpu,cpuacct,memory'
 
 shocker_check() {
   btrfs subvolume list "$btrfs_path" | grep -qw "$1"
@@ -46,15 +47,15 @@ addr_to_hostid() { #Transforms ip/mask into an int representing the host
 }
 
 get_state() {
-  [[ ! -d "$btrfs_path/$1" ]] && echo missing && return
-  [[ -d "/sys/fs/cgroup/cpuacct/$1" ]] && cgdef=1 || cgdef=0
+  [ ! -d "$btrfs_path/$1" ] && echo missing && return
+  [ -d "/sys/fs/cgroup/cpuacct/$1" ] && cgdef=1 || cgdef=0
   grep -q . "/sys/fs/cgroup/cpuacct/$1/tasks" 2>/dev/null && procs=1 || procs=0
   ip netns show | grep -q "netns_$1" 2>/dev/null && netns=1 || netns=0
   ip link show | grep -q "veth0_$1" 2>/dev/null && veth=1 || veth=0
 
   state=crashed
-  [[ $((cgdef & procs & netns & veth)) -eq 1 ]] && state=running
-  [[ $((cgdef | procs | netns | veth)) -eq 0 ]] && state=stopped
+  [ $((cgdef & procs & netns & veth)) -eq 1 ] && state=running
+  [ $((cgdef | procs | netns | veth)) -eq 0 ] && state=stopped
 
   echo $state
 }
@@ -67,4 +68,39 @@ shocker_execute() {
     unshare -fmuip --mount-proc \
     chroot "$btrfs_path/$cntid" \
     /bin/sh -c "source /root/init; $*" || true
+}
+
+get_base_network () {
+  . ./settings.conf 2> /dev/null
+  echo "${NETWORK:-10.0.0.0/24}"
+}
+
+get_mask () {
+  NETWORK="$(get_base_network)"
+  # shellcheck disable=SC2001
+  echo "$NETWORK" | sed 's#^.*/##g'
+}
+
+get_nhosts () {
+  MASK=$(get_mask)
+  echo $((2**(32-MASK)))
+}
+
+get_network () {
+  addr_to_network "$(get_base_network)"
+}
+
+get_gateway () {
+  NETWORK=$(get_network)
+  echo $((NETWORK + 1))
+}
+
+gen_uuid() {
+  find "$btrfs_path" -maxdepth 1 -type d -name 'ps_*' \
+    | sed 's#^.*/ps_##' \
+    | xargs printf "%010d\n" \
+    | sort \
+    | comm -1 -3 - <(seq -f "%010g" 2 "$1") \
+    | head -1 \
+    | sed 's/^0*//'
 }
